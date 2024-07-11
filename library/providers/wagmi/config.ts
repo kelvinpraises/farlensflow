b1f072868f3c7ea58223ee5c632145353fa1f352
kelvinpraises <kelvinpraises@gmail.com>
2024-07-11 10:56:11 +0100

    feat: add anvil localhost support

--- library/providers/wagmi/config.ts
+++ library/providers/wagmi/config.ts
@@ -1,19 +1,19 @@
 import { getDefaultConfig } from "connectkit";
 import { createConfig, http } from "wagmi";
-import { mainnet, baseSepolia, sepolia, localhost } from "wagmi/chains";
+import { mainnet, baseSepolia, sepolia, anvil } from "wagmi/chains";
 
 export const config = createConfig(
   getDefaultConfig({
     appName: "Farlensflow",
     walletConnectProjectId:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-    chains: [mainnet, sepolia, baseSepolia, localhost],
+    chains: [mainnet, sepolia, baseSepolia, anvil],
     multiInjectedProviderDiscovery: true,
     transports: {
       [mainnet.id]: http(),
       [sepolia.id]: http(),
       [baseSepolia.id]: http(),
-      [localhost.id]: http(),
+      [anvil.id]: http(),
     },
   }),
 );
