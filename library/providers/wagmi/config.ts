71b8cbeae636828b4bd8abb412551971b83e93ec
kelvinpraises <kelvinpraises@gmail.com>
2024-07-11 07:28:02 +0100

    feat: add localhost to chains

--- library/providers/wagmi/config.ts
+++ library/providers/wagmi/config.ts
@@ -1,18 +1,19 @@
 import { getDefaultConfig } from "connectkit";
 import { createConfig, http } from "wagmi";
-import { mainnet, baseSepolia, sepolia } from "wagmi/chains";
+import { mainnet, baseSepolia, sepolia, localhost } from "wagmi/chains";
 
 export const config = createConfig(
   getDefaultConfig({
     appName: "Farlensflow",
     walletConnectProjectId:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-    chains: [mainnet, sepolia, baseSepolia],
+    chains: [mainnet, sepolia, baseSepolia, localhost],
     multiInjectedProviderDiscovery: true,
     transports: {
       [mainnet.id]: http(),
       [sepolia.id]: http(),
       [baseSepolia.id]: http(),
+      [localhost.id]: http(),
     },
   }),
 );
