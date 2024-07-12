a07b1011445be816c4ea6c5722c0b98dc0f97e88
kelvinpraises <kelvinpraises@gmail.com>
2024-07-12 09:41:54 +0100

    chore: add min-height

--- library/components/molecules/glass-container.tsx
+++ library/components/molecules/glass-container.tsx
@@ -2,8 +2,8 @@ import React from "react";
 
 const GlassContainer = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center w-full">
-      <div className=" backdrop-blur-3xl bg-black/30 rounded-3xl p-2 m-4 w-full max-w-[700px]">
+    <div className="flex justify-center w-full min-h-[640px]">
+      <div className="flex flex-col backdrop-blur-3xl bg-black/30 rounded-3xl p-2 m-4 w-full max-w-[700px]">
         {children}
       </div>
     </div>
