668a1b605c98aeeab0b4e8ed7b09ae6ca5b767d2
kelvinpraises <kelvinpraises@gmail.com>
2024-07-12 09:37:38 +0100

    feat: update sematic tags and add image

--- app/page.tsx
+++ app/page.tsx
@@ -5,18 +5,25 @@ import GlassContainer from "@/components/molecules/glass-container";
 const IntentsHome = async () => {
   return (
     <GlassContainer>
-      <p className="pt-4 px-4 font-semibold text-xl">Home</p>
-      <p className="px-4 pb-2 text-sm text-gray-700">
-        Fuel shared initiatives with decentralised funding flows
-      </p>
-      <div className="rounded-2xl bg-[#F8F8F7] p-4">
-        <Image
-          alt="query intents"
-          src="/senti-dripz-tldr.svg"
-          width={900}
-          height={300}
-        />
-      </div>
+      <header>
+        <h1 className="pt-4 px-4 font-semibold text-xl">Home</h1>
+        <p className="px-4 pb-2 text-sm text-gray-700">
+          Fuel shared initiatives with decentralised funding flows
+        </p>
+      </header>
+      <main className="rounded-2xl bg-[#F8F8F7] p-4 flex-1">
+        <div className="flex flex-col items-center">
+          <Image
+            alt="lens"
+            src="https://illustrations.popsy.co/red/photographer.svg"
+            width={400}
+            height={400}
+          />
+          <h2 className="text-sm text-center sm:text-xl text-black">
+            Peer into the social depths and unlock the power of flows
+          </h2>
+        </div>
+      </main>
     </GlassContainer>
   );
 };
