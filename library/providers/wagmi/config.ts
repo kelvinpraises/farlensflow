d5e26f72eadf6269195c220f45ab3672d9e9aa23
kelvinpraises <kelvinpraises@gmail.com>
2024-07-12 20:26:47 +0100

    chore: remove unsupported networks

--- library/providers/wagmi/config.ts
+++ library/providers/wagmi/config.ts
@@ -1,17 +1,15 @@
 import { getDefaultConfig } from "connectkit";
 import { createConfig, http } from "wagmi";
-import { mainnet, baseSepolia, sepolia, anvil } from "wagmi/chains";
+import { anvil, baseSepolia } from "wagmi/chains";
 
 export const config = createConfig(
   getDefaultConfig({
     appName: "Farlensflow",
     walletConnectProjectId:
       process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
-    chains: [mainnet, sepolia, baseSepolia, anvil],
+    chains: [baseSepolia, anvil],
     multiInjectedProviderDiscovery: true,
     transports: {
-      [mainnet.id]: http(),
-      [sepolia.id]: http(),
       [baseSepolia.id]: http(),
       [anvil.id]: http(),
     },
